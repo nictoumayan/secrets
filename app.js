bf19303c6c6b2db74bedffa785562f4495b04cbe
Nic <user@USER-no-MacBook-Pro-2.local>
2020-10-01 08:04:15 +0900

    add md5 encryption to passwords

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const app = express();
+
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -17,7 +20,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
 
 
 const User = new mongoose.model("User", userSchema);
@@ -47,7 +50,7 @@ app.get("/logout", function(req,res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
   if(err){
@@ -60,7 +63,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email:userName}, function(err, foundUser){
     if(err){
       res.render("error");
