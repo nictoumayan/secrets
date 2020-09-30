b2f088c0ead1f872d2b468af60ff3c1758c96b41
Nic <user@USER-no-MacBook-Pro-2.local>
2020-09-30 21:35:54 +0900

    add mongoose-encryption to password value

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const encrypt = require("mongoose-encryption");
 const app = express();
 
 app.use(express.static("public"));
@@ -11,10 +11,15 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewURLParser:true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema);
 
