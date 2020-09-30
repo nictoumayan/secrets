188bfc98c61408f9fd0964842394be0aa2ab38b9
Nic <user@USER-no-MacBook-Pro-2.local>
2020-09-30 21:18:37 +0900

    establish database connection, level 1 authentication complete

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewURLParser:true});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+//---------------------gets------------------------------
 
 app.get("/", function(req,res){
   res.render("home");
@@ -18,6 +31,39 @@ app.get("/login", function(req,res){
 
 app.get("/register", function(req,res){
   res.render("register");
+});
+
+//------------------------posts---------------------------
+
+
+app.post("/register", function(req,res){
+  const newUser = new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+  if(err){
+    console.log(err);
+  }else{
+    res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req,res){
+  const userName = req.body.username;
+  const password = req.body.password;
+  User.findOne({email:userName}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+  })
 });
 
 app.listen(3000, function(){
