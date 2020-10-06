505263083cf90b8d3d7db16159346bb2e2568a4b
Nic <user@USER-no-MacBook-Pro-2.local>
2020-10-06 14:20:51 +0900

    secrets page now renders user-submitted secrets

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ const userSchema = new mongoose.Schema({
   username: {type: String, sparse: true},
   email: String,
   password: String,
+  secret: String,
   provider: String,
   googleId: String,
   facebookId: String
@@ -165,12 +166,24 @@ app.get("/logout", function(req, res) {
 });
 
 app.get("/secrets", function(req, res) {
+  User.find({"secret": {$ne:null}}, function(err, foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
-});
+})
 
 
 //------------------------posts---------------------------
@@ -206,6 +219,26 @@ app.post("/login", function(req, res) {
     }
   })
 });
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if (foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
+
+
+
 
 app.listen(3000, function() {
   console.log("server listening on port 3000");
